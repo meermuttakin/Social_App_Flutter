1ed1c34dacb0fbc8ee47b7d773af01bcdecb8367
meermuttakin <meermuttakin@gmail.com>
2020-04-13 13:04:27 +0600

    Adding Search User

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,7 +1,11 @@
-import 'dart:html';
+//import 'dart:html';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -9,19 +13,35 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  AppBar buildSearchField(){
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResultsFuture;
+  handleSearch (String query){
+    Future<QuerySnapshot> users = userRef.where("displayName",
+        isGreaterThanOrEqualTo: query)
+        .getDocuments();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+  clearSearch(){
+    searchController.clear();
+  }
+
+  AppBar buildSearchField (){
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
           hintText: "Search for a user",
           filled: true,
           prefixIcon: Icon(Icons.account_box, size: 28.0),
           suffixIcon: IconButton(
               icon: Icon(Icons.clear),
-              onPressed: () => print('cleared'),
+              onPressed: clearSearch,
           ),
         ),
+        onFieldSubmitted: handleSearch,
       ),
     );
   }
@@ -34,7 +54,7 @@ class _SearchState extends State<Search> {
           shrinkWrap: true,
           children: <Widget>[
             SvgPicture.asset('assets/images/search.svg',
-            height: orientation == Orientation.portrait ? 300.0 : 200.0,),
+            height: orientation == Orientation.portrait ? 300.0 : 200.0),
             Text("Find Users",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -42,11 +62,30 @@ class _SearchState extends State<Search> {
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.w600,
               fontSize: 60.0,
-            ),
+             ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  buildsearchResult (){
+    return FutureBuilder(
+      future: searchResultsFuture,
+      builder: (context,snapshot){
+        if(!snapshot.hasData){
+          return circularProgress();
+        }
+        List<Text> searchResults = [];
+        snapshot.data.documents.forEach((doc){
+          User user = User.fromDocument(doc);
+          searchResults.add(Text(user.username));
+        });
+        return ListView(
+          children: searchResults,
+        );
+      },
     );
   }
 
@@ -55,7 +94,7 @@ class _SearchState extends State<Search> {
     return new Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
-      body: buildNoContent(),
+      body: searchResultsFuture == null ? buildNoContent() : buildsearchResult,
     );
   }
 }
